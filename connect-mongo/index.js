133a814f88e012b5832e9779d410a438ba939fec
BaneSkillix <71550726+BaneSkillix@users.noreply.github.com>
2021-02-10 19:58:05 +0100

    Adding Library mongoose

---
+++ connect-mongo/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const _ = require('lodash');
+
+const clients = {};
+let connectionTimeout;
+
+module.exports.init = () => {};
+
+module.exports.closeConnections = () => {};
+
+module.exports.getClients = () => {};
